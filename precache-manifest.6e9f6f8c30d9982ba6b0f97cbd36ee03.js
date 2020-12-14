4d41f1462d36a5221bb4d78ffe4c73875a79640b
JayanOzhi <jkandathil@stratuscent.com>
2020-12-14 00:47:02 -0500

    Updates

---
+++ precache-manifest.6e9f6f8c30d9982ba6b0f97cbd36ee03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fb9a5d81c900eea4fd4f82613e85af",
+    "url": "/aqfume.git/index.html"
+  },
+  {
+    "revision": "9b302ded1709b10096a5",
+    "url": "/aqfume.git/static/css/main.6322a2d0.chunk.css"
+  },
+  {
+    "revision": "207de6c353043265cc6e",
+    "url": "/aqfume.git/static/js/2.2884e0f7.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/aqfume.git/static/js/2.2884e0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b302ded1709b10096a5",
+    "url": "/aqfume.git/static/js/main.fd094d2d.chunk.js"
+  },
+  {
+    "revision": "96f07df6228d0205c23a",
+    "url": "/aqfume.git/static/js/runtime-main.8857fe4c.js"
+  }
+]);
